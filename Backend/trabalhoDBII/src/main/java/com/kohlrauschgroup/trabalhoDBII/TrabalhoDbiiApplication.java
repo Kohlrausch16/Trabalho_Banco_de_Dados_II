410aa40fdf3b7ca6d09b592c40e9bab70aa59965
Kohlrausch <joaohenriquekohlrausch@gmail.com>
2025-11-18 13:44:17 -0300

    Initial commit

---
+++ Backend/trabalhoDBII/src/main/java/com/kohlrauschgroup/trabalhoDBII/TrabalhoDbiiApplication.java
@@ -0,0 +1,13 @@
+package com.kohlrauschgroup.trabalhoDBII;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrabalhoDbiiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrabalhoDbiiApplication.class, args);
+	}
+
+}
